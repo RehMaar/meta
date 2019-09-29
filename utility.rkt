d4f33ab55749a652d0af82b1831743bd04f25c8f
Maria Kuklina <mkdirwtf@gmail.com>
2019-09-29 22:19:51 +0300

    Add lots of helpers

--- utility.rkt
+++ utility.rkt
@@ -30,7 +30,38 @@
   (let ([value (findf (lambda (key-value) (eq? key (car key-value))) dict)])
     (if value
       (cdr value)
-      (error "Couldn't find key in the given dict." key)
+      ;(error "Couldn't find key in the given dict." key)
+      key
     )
   )
 )
+
+(define (key? dict key)
+  (let ([value (findf (lambda (key-value) (eq? key (car key-value))) dict)])
+    (if value #t #f))
+)
+
+(define (pair fst snd) `(,fst . ,snd))
+
+(define (unite lst1 lst2) (append lst1 lst2))
+
+(define (elem? x xs)
+  (cond
+    [(null? xs) #f]
+    [(equal? x (car xs)) #t]
+    [else (elem? x (cdr xs))]
+))
+
+(define (update dict key value)
+	(cond
+    [(null? dict) (cons (pair key value) dict)]
+    [(equal? key (caar dict)) (cons (pair key value) (cdr dict))]
+    [else (cons (car dict) (update (cdr dict) key value))]
+  )
+)
+
+(define (second f p)
+  (match p
+    [`(,fst . ,snd) `(,fst . ,(f snd))]
+    [else (error "second: argument is not a pair!")]
+  ))
