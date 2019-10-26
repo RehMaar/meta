f098a847266824a2ea4e6b3cce4df576b194f931
Maria Kuklina <mkdirwtf@gmail.com>
2019-10-26 22:48:39 +0300

    Add II proj

--- fc_fproj.rkt
+++ fc_fproj.rkt
@@ -1,15 +1,17 @@
 (load "fc_mix0.rkt")
-;(load "fc_mix1.rkt")
+(load "fc_mix1.rkt")
 (load "tm_intrp.rkt")
-; 
+
+;
+; I Futamura projection
+;
 (define (fc-mix-fp1 div vs0)
-  (run-mix0 (list tm-intrp div_tm vs0_tm)))
+  (run-mix0 (list tm-intrp div vs0)))
 
 ;
 ; Test FP1
 ;
-
-(define div_tm (list
+(define div-tm (list
   'prog
   'next-label
   'instr
@@ -21,44 +23,53 @@
   '(not (has-label next-label prog))
 ))
 
-(define vs0_tm (list
+(define vs0-tm (list
   (pair 'prog tm-prog)))
 
-;(define fc-mix-fp1-test
-;  (fc-mix-fp1 div_tm vs0_tm))
+; test
+(define fc-mix-fp1-test
+  (fc-mix-fp1 div-tm vs0-tm))
+
+; pretty printed test
+(define fc-mix-fp1-test-pp
+  (pretty-print (fc-mix-fp1 div-tm vs0-tm)))
+
 ;
-;(define fc-mix-fp1-test-pp
-;  (pretty-print (fc-mix-fp1 div_tm vs0_tm)))
-
-
-(define (fc-mix-fp1 div vs0)
-  (run-mix0 (list tm-intrp div_tm vs0_tm)))
-
+; II Futamura projection
+;
+; comp = [[mix {for} fc {on} fc]]
+;         [ mix {for} fc {in} fc
+;         , div {for} mix {fc, fc}
+;         , vs_0 = [ program -- int
 (define div-mix (list
 	'program
   'div
 ))
 
 (define vs-mix (list
-  (pair 'div div_tm)
-  (pair 'vs0 vs0_tm)
+  (pair 'program tm-intrp)
+  (pair 'div div-tm)
 ))
 
-;(define fc-mix-fp2
-;  (run-mix1 (list fc-mix1 div-mix vs-mix))
-;)
 
-
-(define p
-'((read tape-right)
-  ((label 3)
-   (:= tape-left '())
-   (if (eq? (tape-head tape-right) 0) (label 2) (label 1)))
-  ((label 2)
-   (:= tape-right (tape-write 1 tape-right))
-   (return (cons tape-left tape-right)))
-  ((label 1)
-   (:= tape-left (cons (tape-head tape-right) tape-left))
-   (:= tape-right (tape-tail tape-right))
-   (if (eq? (tape-head tape-right) 0) (label 2) (label 1))))
+(define fc-mix-fp2
+  (run-mix1 (list fc-mix1 div-mix vs-mix))
 )
+ 
+(define fc-mix-fp2-pp
+  (pretty-print (run-mix1 (list fc-mix1 div-mix vs-mix)))
+)
+
+;
+; *Check that this is really a compiler.
+;
+(define fc-mix-fp2-test-pp
+  (pretty-print (intrp fc-mix-fp2-pp (list vs0-tm)))
+)
+
+;
+; fc-mix-fp2-test-pp and fc-mix-fp1-test-pp are equal.
+
+
+
+
